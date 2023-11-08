c2350be059b2ff1a660912add2f5530ee23bb2af
Dewald <dewaldfourie0808@gmail.com>
2023-11-08 16:18:03 +0200

    added cart functionality and designs

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,29 +1,91 @@
+import { useState } from 'react'
 import './styles/cart.css'
 import PropTypes from 'prop-types'
+import CartItem from './CartItem'
 
 const Cart = ({ isOpen, closeCart }) => {
+
+    const [demoItems, setDemoItems] = useState([
+        {
+            id: 1,
+            imageSrc: 'item1.jpg',
+            name: 'Item 1',
+            size: 'M',
+            quantity: 1,
+            price: 45.00,
+        },
+        {
+            id: 2,
+            imageSrc: 'item2.jpg',
+            name: 'Item 2',
+            size: 'L',
+            quantity: 1,
+            price: 30.00,
+        },
+        {
+            id: 3,
+            imageSrc: 'item3.jpg',
+            name: 'Item 3',
+            size: 'S',
+            quantity: 1,
+            price: 25.00,
+        },
+    ]);
+
+    const calculateTotalQuantity = (items) => {
+        return items.reduce((total, item) => total + item.quantity, 0);
+    }
+
+    const calculateTotalPrice = (items) => {
+        return items.reduce((total, item) => total + item.quantity * item.price, 0)
+    } 
+
+    const [totalItems, setTotalItems] = useState(calculateTotalQuantity(demoItems));
+    const [totalPrice, setTotalPrice] = useState(calculateTotalPrice(demoItems));
+
+    const updateTotalItems = (updatedItems) => {
+        setDemoItems(updatedItems)
+        setTotalItems(calculateTotalQuantity(updatedItems))
+        setTotalPrice(calculateTotalPrice(updatedItems))
+    }
+
+    const removeItem = (itemToRemove) => {
+        const updatedItems = demoItems.filter((item) => item.id !== itemToRemove.id);
+        setDemoItems(updatedItems);
+        setTotalItems(calculateTotalQuantity(updatedItems));
+        setTotalPrice(calculateTotalPrice(updatedItems))
+    }
+
     return (
         <div className={`cart ${isOpen ? 'open' : ''}`}>
             <div className='cart-left'>
                 <div className='cart-header left'>
                     <h2>Shopping Cart</h2>
-                    <span className='number-of-items'>#items</span>
+                    <span className='number-of-items'>{totalItems} Items</span>
+                    <button className='close-cart-mobile' onClick={closeCart}>X</button>
                 </div>
                 <hr />
                 <div className='cart-content'>
-                    {/* will be a dynamically generated items */}
-                    <div className='cart-item'>
-                        <span>ITEM 1</span>
-                    </div>
-                    <hr />
-                    <div className='cart-item'>
-                        <span>ITEM 2</span>
-                    </div>
-                    <hr />
-                    <div className='cart-item'>
-                        <span>ITEM 3</span>
-                    </div>
-                    <hr />
+                    {demoItems.length === 0 ? (
+                        <div className='cart-empty-container'>
+                            <h3>Sorry, The Cart is Empty</h3>
+                            <h4>Go to the Shop to fill it up!</h4>
+                        </div>
+                    ) : (
+                        demoItems.map((item) => (
+                            <div className='cart-item-container' key={item.id}>
+                                <CartItem
+                                    item={item} 
+                                    updateTotalItems={updateTotalItems} 
+                                    demoItems={demoItems} 
+                                    removeItem={removeItem}
+                                />
+                            </div>
+                        ))
+                    )}
+                    
+
+                    
                 </div>
                 <div className='closeCart-btn-container'>
                     <button onClick={closeCart}>Back to Shop</button>
@@ -35,8 +97,8 @@ const Cart = ({ isOpen, closeCart }) => {
                 </div>
                 <hr />
                 <div className='total-items'>
-                    <h4>#ITEMS</h4>
-                    <h4>$120.00</h4>
+                    <h4>ITEMS: {totalItems}</h4>
+                    <h4>$ {totalPrice.toFixed(2)}</h4>
                 </div>
                 <div className='shipping-container'>
                     <h4>SHIPPING</h4>
@@ -53,7 +115,7 @@ const Cart = ({ isOpen, closeCart }) => {
                 <hr />
                 <div className='total-price-container'>
                     <h4>TOTAL PRICE</h4>
-                    <h4>$125.00</h4>
+                    <h4>$ {totalPrice.toFixed(2)}</h4>
                 </div>
                 <div className='checkout-btn-container'>
                     <button>CHECKOUT</button>
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types'
+import './styles/cart.css'
+
+const CartItem = ({ item , updateTotalItems, demoItems, removeItem}) => {
+
+    const [quantity, setQuantity] = useState(item.quantity || 1);
+
+    const incrementItemQuantity = (itemId) => {
+        return demoItems.map((i) => {
+            if (i.id === itemId) {
+                return { ...i, quantity: i.quantity + 1 };
+            }
+            return i;
+        });
+    };
+
+    const incrementQuantity = () => {
+        setQuantity(quantity + 1)
+        updateTotalItems(incrementItemQuantity(item.id))
+    }; 
+
+    const decrementItemQuantity = (itemId) => {
+        return demoItems.map((i) => {
+            if (i.id === itemId) {
+                return { ...i, quantity: i.quantity - 1 };
+            }
+            return i;
+        });
+    };
+
+    const decrementQuantity = () => {
+        if (quantity > 1) {
+            setQuantity(quantity - 1)
+            updateTotalItems(decrementItemQuantity(item.id))
+        }
+    };
+
+    const handleInputChange = (e) => {
+        // Prevent direct input into the input field
+        e.preventDefault();
+    };
+
+    const removeCartItem = () => {
+        const updatedItems = demoItems.filter((i) => i.id !== item.id);
+        updateTotalItems(updatedItems);
+        removeItem(item)
+    }
+
+    return (
+        <div className='cart-item'>
+            <div className='item-image-container'>
+                <img className="cart-item-image" src="" alt={item.imageSrc} />
+            </div>
+            <div className='item-details-container'>
+                <span className='item-name'>{item.name}</span>
+                <div className='item-info'>
+                    <span>Size: </span>
+                    <span>{item.size}</span>
+                </div>
+            </div>
+            <div className='item-quantity-container'>
+                {/* <div className='item-quantity-text'>
+                    <span>Qty:</span>
+                </div> */}
+                <div className='item-quantity-display'>
+                    <button onClick={decrementQuantity}>-</button>
+                    <input 
+                        className="quantity-input" 
+                        type="number" value={quantity} 
+                        onChange={handleInputChange} 
+                        readOnly    
+                    />
+                    <button onClick={incrementQuantity}>+</button>
+                </div>
+            </div>
+            <div className='item-price-container'>
+                <span>$ {(item.price * item.quantity).toFixed(2)}</span>
+            </div>
+            <div className='item-remove-container'>
+                <button className='item-remove-button' onClick={removeCartItem}>x</button>
+            </div>
+        </div>
+    )
+}
+
+CartItem.propTypes = {
+    item: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        size: PropTypes.string.isRequired,
+        quantity: PropTypes.number,
+        price: PropTypes.number.isRequired,
+        imageSrc: PropTypes.string.isRequired,
+    }).isRequired,
+    updateTotalItems: PropTypes.func.isRequired, 
+    demoItems: PropTypes.array.isRequired, 
+    removeItem: PropTypes.func.isRequired,
+};
+
+
+export default CartItem
